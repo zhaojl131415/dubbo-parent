96a755b7b7c3cbeab318bb42c529e4a19d43454c
zhaojinliang <zhaojl131415@163.com>
2020-06-01 09:31:59 +0800

    Initial commit

---
+++ dubbo-common/src/main/java/org/apache/dubbo/common/extension/factory/AdaptiveExtensionFactory.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.dubbo.common.extension.factory;
+
+import org.apache.dubbo.common.extension.Adaptive;
+import org.apache.dubbo.common.extension.ExtensionFactory;
+import org.apache.dubbo.common.extension.ExtensionLoader;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * AdaptiveExtensionFactory
+ */
+@Adaptive
+public class AdaptiveExtensionFactory implements ExtensionFactory {
+
+    /**
+     * @see SpiExtensionFactory
+     * @see org.apache.dubbo.config.spring.extension.SpringExtensionFactory
+     */
+    private final List<ExtensionFactory> factories;
+
+    public AdaptiveExtensionFactory() {
+        /**
+         * 扩展点加载器支持哪些ExtensionFactory
+         * @see SpiExtensionFactory
+         * @see org.apache.dubbo.config.spring.extension.SpringExtensionFactory
+         */
+        ExtensionLoader<ExtensionFactory> loader = ExtensionLoader.getExtensionLoader(ExtensionFactory.class);
+        List<ExtensionFactory> list = new ArrayList<ExtensionFactory>();
+        for (String name : loader.getSupportedExtensions()) {
+
+            list.add(loader.getExtension(name));
+        }
+        factories = Collections.unmodifiableList(list);
+    }
+
+    @Override
+    public <T> T getExtension(Class<T> type, String name) {
+        // 遍历ExtensionFactory
+        for (ExtensionFactory factory : factories) {
+            /**
+             * @see org.apache.dubbo.config.spring.extension.SpringExtensionFactory#getExtension(java.lang.Class, java.lang.String)
+             * @see SpiExtensionFactory#getExtension(java.lang.Class, java.lang.String)
+             */
+            T extension = factory.getExtension(type, name);
+            if (extension != null) {
+                return extension;
+            }
+        }
+        return null;
+    }
+
+}
